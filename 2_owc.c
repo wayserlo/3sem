735669f460b8270db3610dae1155881539083d7c
wayserlo <astanova.ks@phystech.edu>
2020-12-09 20:34:24 +0300

    fixed indents in 2_owc.c

--- 2_owc.c
+++ 2_owc.c
@@ -5,32 +5,29 @@
 #include <string.h>
 #include <stdio.h>
 
-int 
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	if (argc < 2) {
-		printf("Enter the file name\n");
-		return 1;
-	}
-	int		fd;
-	fd = open(argv[1], O_RDWR | O_CREAT | O_APPEND, 00644);
-	if (fd == -1) {
-		perror("Failed to open");
-		return 2;
-	}
+    if (argc < 2) {
+        printf("Enter the file name\n");
+        return 1;
+    }
+    int fd;
+    fd = open(argv[1], O_RDWR | O_CREAT | O_APPEND, 00644);
+    if (fd == -1) {
+        perror("Failed to open");
+        return 2;
+    }
     if (write(fd, argv[2], strlen(argv[2])) == -1) {
-				perror("Failed to write");
-                if (close(fd) != 0) {
-                    perror("Failed to close");
-                    return 3;
-                }
-				return 4;
-			}
-
-	if (close(fd) != 0) {
-		perror("Failed to close");
-		return 3;
-	}
-	return 0;
-
+        perror("Failed to write");
+        if (close(fd) != 0) {
+            perror("Failed to close");
+            return 3;
+        }
+        return 4;
+    }
+    if (close(fd) != 0) {
+        perror("Failed to close");
+        return 3;
+    }
+    return 0;
 }
