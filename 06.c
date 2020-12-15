c7ee4710785783874266a90e5efe11a0267ba5ac
wayserlo <astanova.ks@phystech.edu>
2020-12-16 02:32:59 +0300

    fixed output type in case "unknown"

--- 06.c
+++ 06.c
@@ -1,6 +1,10 @@
+#include <sys/stat.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdio.h>
+#include <string.h>
+
 const char *dtype_description(unsigned d_type) {
     switch (d_type) {
         case DT_BLK:
@@ -22,6 +26,28 @@ const char *dtype_description(unsigned d_type) {
     }
 }
 
+const char *ftype_description(mode_t f_type) {
+    switch (f_type) {
+        case S_IFBLK:
+            return "block device";
+        case S_IFCHR:
+            return "character device";
+        case DT_DIR:
+            return "directory";
+        case S_IFIFO:
+            return "FIFO/pipe";
+        case S_IFLNK:
+            return "symbolic link";
+        case S_IFREG:
+            return "regular file";
+        case S_IFSOCK:
+            return "UNIX domain socket";
+        default:
+            return "unknown";
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
     //проверяем, что ввели название
@@ -39,9 +65,20 @@ int main(int argc, char *argv[])
 
     //читаем записи и выводим
     struct dirent * list;
+    struct stat stat_buf;
+    char str[]="unknown";
     while((list = readdir(dir)) != NULL) {
-        printf("%-20s %s\n", dtype_description(list->d_type), list->d_name);
-    }    
+        if(strcmp(dtype_description(list->d_type), str) != 0) {
+            printf("%-20s %s\n", dtype_description(list->d_type), list->d_name);
+        } else {
+            if (lstat(list->d_name, &stat_buf) == -1) {
+                perror("Failed to stat");
+                return 2;
+            }
+            printf("%-20s %s\n", ftype_description(stat_buf.st_mode & S_IFMT), list->d_name);
+        }
+    }
+
     //закрываем
     if(closedir(dir) == -1) {
         perror("Failed to close directory");
