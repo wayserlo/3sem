2cd79cea2f6525db226c2c52e2367813591d33c3
wayserlo <astanova.ks@phystech.edu>
2020-12-20 19:07:43 +0300

    add 13.c and fixed 11.c

--- 11.c
+++ 11.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #define BUFSIZE 256
 
 int main(void) {
@@ -15,7 +16,7 @@ int main(void) {
         perror("Failed to open");
         return 1;
     }
-    //блокируем его эксклюзивной блокировкой, чтобы 
+    //блокируем его эксклюзивной блокировкой, чтобы одновременные процессы не перекрывались 
     if(flock(fd, LOCK_EX) == -1) {
         perror("Failed to block");
         close(fd);
@@ -29,15 +30,20 @@ int main(void) {
         close(fd);
         return 3;
     }
-    printf("%s", count);
+    
     //преобразуем в число и обратно
     int n = atoi(count);
     n += 1;
-    sprintf(count, "%d", n);
+    char* rewrite;
+    if(asprintf(&rewrite, "%d", n) < 0) {
+        perror("Failed to asprintf");
+        close(fd);
+        return 3;
+    }
 
     //пишем
-    if(write(fd, count, sizeof(count)) == -1) {
-        perror("Failed to write");
+    if(pwrite(fd, rewrite, strlen(rewrite), 0) == -1) {
+        perror("Failed to pwrite");
         close(fd);
         return 3;
     }
